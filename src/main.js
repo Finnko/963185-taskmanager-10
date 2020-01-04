e0c6a17a4b8b2a1dd97253e712f1d638d59fcf39
Alex Khab <finko85@gmail.com>
2020-01-04 13:11:40 +0300

    added task model with basic methods

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import BoardComponent from './components/board';
 import BoardController from "./controllers/board";
 import FilterComponent from './components/filter';
 import MenuComponent from './components/menu';
+import TaskModel from './models/task';
 import {generateFilters} from "./mocks/filter";
 import {tasksData} from "./mocks/task";
 import {renderComponent, RenderPosition} from "./utils/render";
@@ -15,6 +16,9 @@ renderComponent(siteMainElement, new FilterComponent(filters), RenderPosition.BE
 
 const boardComponent = new BoardComponent();
 renderComponent(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
+
+const taskModel = new TaskModel();
+taskModel.setTasks(tasksData);
 
 const boardController = new BoardController(boardComponent);
 boardController.render(tasksData);
